37cfb59a570dee55caa2c846c85f4762cdfe473a
johan <johan.audy.24@gmail.com>
2023-01-26 23:39:12 +0100

    debut react

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import Banner from "./Banner.js";
 
-function App(){
-    return <Banner />
+function App() {
+    return (
+       <div>
+            <Banner />
+        </div>
+    )
 }
 
 export default App;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,18 @@
+import logo from '../assets/logo-kasa.png'
 import '../styles/Banner.css'
 
 function Banner() {
-    return <div classeName='Kasa-banner'>
-        <h1>Kasa</h1>
+    const accueil = 'Accueil'
+    const propos = 'A Propos'
+    return (
+        <div className='kasa-banner'>
+            <img src={logo} alt='logo Kasa' className='kasa-logo' />
+            <div classename='banner-nav'>
+                <p className='btn-accueil'>{accueil}</p>
+                <p className='btn-propos'>{propos}</p>
+            </div>
         </div>
+    )
 }
 
 export default Banner

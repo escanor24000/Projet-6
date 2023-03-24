e60eb1352d98dc597f3b23a12e7d767829c3e3dd
André Kouamé <kouameandre43@gmail.com>
2023-03-24 19:25:53 +0100

    resolve bug load carroussel image

---
+++ src/components/ApiImages.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+
+export default function ApiImages(id){
+const [images, setImages] = useState([])
+useEffect(() => {
+    async function getData () {   
+        const response = await fetch('/data/data.json')
+        const data = await response.json()
+        console.log(data)
+        const loge = []
+            data.forEach(function (res) {
+                if (id === res.id) {
+                    loge.push(res)
+                }
+            });
+            if(loge.length>0){
+            setImages(loge[0].pictures);
+        }
+    }
+    getData()
+},[])
+
+return images
+
+}
+
+
--- src/components/Logement.js
+++ src/components/Logement.js
@@ -1,6 +1,6 @@
 import '../styles/Logement.css'
 import { useParams } from 'react-router-dom';
-import Api from './Api'
+import ApiImages from './ApiImages'
 import {useState,useEffect} from "react"
 import Slider from './Module_logement/Slide'
 import Localisation from './Module_logement/Localisation'
@@ -10,30 +10,10 @@ import Description from './Module_logement/Description';
 import Equipement from './Module_logement/Equipement'
 
 function Logement() {
-    const [images, setImages] = useState([]);
-    const data = Api();
     const idUrl = useParams();
-    useEffect(() => {
-        if (data /*!= undefined*/) {
-            
-            const loge = []
-            data.forEach(function (res) {
-                if (idUrl.id === res.id) {
-                    loge.push(res)
-                }
-
-            });
-            if(loge.length>0){
-            setImages(loge[0].pictures);
-            }
-            console.log(data);
-        }
-    }, []);
-
+    const images = ApiImages(idUrl.id);
     return (
-
         <Slider slides={images}/>
-
     )
 }
 export default Logement
